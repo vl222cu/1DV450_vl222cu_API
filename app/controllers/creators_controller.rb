27b9e36197e53aa1b49f25d8331497cbb0057449
Vivi <vl222cu@student.lnu.se>
2015-02-28 13:29:54 +0000

    GET, POST & errorhandling for creator is working nicely

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -4,33 +4,35 @@ class CreatorsController < ApplicationController
   before_action :api_authenticate, only: [:index]
   before_action :offset_params, only: [:index]
   
-  # Get all creators
+  # GET all creators
   def index
     creators = Creator.limit(@limit).offset(@offset).includes(:places)
     respond_with creators, status: :ok, location: creators_path
   end
   
-  # Get a specific creator
+  # GET a specific creator
   def show
     @creator = Creator.find_by_id(params[:id])
-    respond_with @creator, status: :ok, location: creators_path(@creator)
+    if @creator.present?
+      respond_with @creator, status: :ok, location: creators_path(@creator)
+    else
+      render json: {error: 'Could not find the resource. Check if you are using the right creator_id.'}, status: :not_found
+    end
   end
   
-  # Post a creator
+  # POST a creator
   def create
     creator = Creator.new(creator_params)
     if creator.save
       respond_with creator, status: :created
     else
-      respond_with creator.errors, status: :unprocessable_entity
+      render json: {error: 'Could not create the resource. Check if you are using the required parameters.'}, status: :unprocessable_entity
     end
   end
 
   private
 
   def creator_params
-    json_params = ActionController::Parameters.new( JSON.parse(request.body.read) )
-    json_params.require(:creator).permit(:username, :email, :password)
+    params.require(:creator).permit(:username, :email, :password)
   end
-
 end
