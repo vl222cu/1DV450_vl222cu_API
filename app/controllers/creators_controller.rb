53f89082a146b0be4fb0e223e7405dcc0339e7e4
Vivi <vl222cu@student.lnu.se>
2015-02-28 10:20:52 +0000

    testcommittar

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,32 +1,36 @@
 class CreatorsController < ApplicationController
   respond_to :json
-  before_action :api_key
-  before_action :api_authenticate, only: [:create]
+  #before_action :api_key
+  before_action :api_authenticate, only: [:index]
   before_action :offset_params, only: [:index]
   
+  # Get all creators
   def index
     creators = Creator.limit(@limit).offset(@offset).includes(:places)
-    render json: creators, status: :ok
+    respond_with creators, status: :ok, location: creators_path
   end
   
+  # Get a specific creator
   def show
     @creator = Creator.find_by_id(params[:id])
-    respond_with @creator, location: creators_path(@creator)
-  rescue ActiveRecord::RecordNotFound
-
+    respond_with @creator, status: :ok, location: creators_path(@creator)
+  end
+  
+  # Post a creator
   def create
     creator = Creator.new(creator_params)
     if creator.save
-      render json: creator, status: :created
+      respond_with creator, status: :created
     else
-      render json: creator.errors, status: :unprocessable_entity
+      respond_with creator.errors, status: :unprocessable_entity
     end
   end
 
   private
 
   def creator_params
-    params.require(:creator).permit(:username, :email, :password)
+    json_params = ActionController::Parameters.new( JSON.parse(request.body.read) )
+    json_params.require(:creator).permit(:username, :email, :password)
   end
 
 end
